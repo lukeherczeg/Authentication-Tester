185045d660873327216818bf104d0ef790658653
lukeherczeg <lukeherczeg@ufl.edu>
2018-03-21 14:30:05 -0400

    Made it cleaner and leaner

--- src/AuthenticationTest.cpp
+++ src/AuthenticationTest.cpp
@@ -4,17 +4,75 @@
 #include <istream>
 using namespace std;
 
+void signUp(string userName, string passWord){
+	bool exists = false;
+	string auth = "Username: " + userName + "    Password: " + passWord;
+	ifstream readData;
+	string tempUser;
+
+	readData.open("authData.txt");
+	while(getline(readData, tempUser) && !exists){
+		if(tempUser == auth)
+			exists= true;
+	}
+
+	if(!exists){
+		ofstream writeData;
+		writeData.open ("authData.txt", ios_base::app);
+		writeData << auth << endl;
+		cout << "Good stuff! You can now log in." << endl;
+		writeData.close();
+	}
+	else{
+		cout << "That account exists silly! Go log in!" << endl;
+	}
+}
+
+void logIn(string userName, string passWord, bool & finished){
+	string tempUser;
+	bool login = false;
+	ifstream readData;
+	string auth = "Username: " + userName + "    Password: " + passWord;
+
+	readData.open("authData.txt");
+	while(getline(readData, tempUser) && !login){
+		if(tempUser == auth)
+			login = true;
+	}
+	if(login){
+		cout << "You're in! :)" << endl;
+		finished = true;
+	}
+	else
+		cout << "Didn't find your username. Please sign up." << endl;
+	readData.close();
+}
+
+void printUsers(){
+	ifstream readData;
+	string tempUser;
+	int count = 0;
+
+	cout << "Users:\n" << endl;
+	readData.open("authData.txt");
+	while(getline(readData, tempUser)){
+		count++;
+		cout << count << ".) " << tempUser << endl;
+	}
+	readData.close();
+}
+
+
+
+
+
+
+
 int main() {
 	string userName = "";
 	string passWord = "";
 	int choice = 0;
 	bool finished = false;
-	ifstream readData;
-	ofstream writeData;
-	string auth;
-	string individualUser;
-	string authContents;
-	bool login = false;
 
 	cout << "Welcome to the Authentication Test. Sign up below! \nIf you have an account already, please enter your username and password.\n" << endl;
 	cout << "1. Log in\n2. Sign up\n3. Print users" << endl;
@@ -23,42 +81,19 @@ int main() {
 		cin >> choice;
 		switch(choice){
 			case 1:
-				cout << "Username : ";
-				cin >> userName;
-				cout << "Password : ";
-				cin >> passWord;
-				readData.open("authData.txt");
-				auth = userName + passWord;
-				while(getline(readData, individualUser) && !login){
-					if(individualUser == auth)
-						login = true;
-				}
-				if(login){
-					cout << "You're in! :)" << endl;
-					finished = true;
-				}
-				else
-					cout << "Didn't find your username. Please sign up." << endl;
-				readData.close();
+				cout << "Username : "; cin >> userName;
+				cout << "Password : "; cin >> passWord;
+
+				logIn(userName, passWord, finished);
 				break;
 			case 2:
-				cout << "Username : ";
-				cin >> userName;
-				cout << "Password : ";
-				cin >> passWord;
-				auth = userName + passWord;
-				writeData.open ("authData.txt", ios_base::app);
-				writeData << auth << endl;
-				cout << "Good stuff! You can now log in." << endl;
-				writeData.close();
+				cout << "Username : "; cin >> userName;
+				cout << "Password : "; cin >> passWord;
+
+				signUp(userName, passWord);
 				break;
 			case 3:
-				cout << "Users:\n" << endl;
-				readData.open("authData.txt");
-				while(getline(readData, individualUser)){
-					cout << individualUser << endl;
-				}
-				readData.close();
+				printUsers();
 				break;
 			case 4:
 				finished = true;
